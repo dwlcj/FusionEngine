1f2a94734da7a2b4fff90e043eaddd1aab2a99d2
perukas <ankarako@iti.com>
2018-11-26 16:52:09 +0200

    + added header-only deps (cli, plog, imgui, rxcpp) ~ begun implementing Filesystem Project

---
+++ FusionEngine/include/filesystem/filesystem.h
@@ -0,0 +1,17 @@
+#ifndef _INCLUDE_FILESYSTEM_FILESYSTEM_H_
+#define _INCLUDE_FILESYSTEM_FILESYSTEM_H_
+
+
+
+namespace fs {
+	// Filesystem class
+	class Filesystem {
+	public:
+		explicit Filesystem() { }
+		// What do I want to do?
+		// Load 3D files
+		// Load Video files
+		// Load/Save Project's state
+	};
+}	// !namespace fs
+#endif	//!_INCLUDE_FILESYSTEM_FILESYSTEM_H_
