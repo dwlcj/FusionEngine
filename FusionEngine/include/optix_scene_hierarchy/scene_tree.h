354209ad647f80498fef40441759285ad7a5a52c
perukas <ankarako@iti.com>
2018-12-07 17:08:41 +0200

    slowly building the SceneTree

--- FusionEngine/include/optix_scene_hierarchy/scene_tree.h
+++ FusionEngine/include/optix_scene_hierarchy/scene_tree.h
@@ -6,10 +6,14 @@
 #include <plog/Log.h>
 /// Fusion
 #include <scene/scene.h>
+#include <ptx_compiler/ptx_compiler.h>
 #include <optix_scene_hierarchy/scene_root.h>
+#include <optix_rendering_system/bounding_box_program.h>
+#include <optix_rendering_system/intersection_program.h>
 /// STL
 #include <vector>
 #include <functional>
+#include <memory>
 namespace tree {
 	/**
 	*	Scene Graph
@@ -20,6 +24,9 @@ namespace tree {
 		explicit SceneTree(optix::Context& ctx)
 			: mContext(ctx)
 		{ }
+		explicit SceneTree(
+			optix::Context& ctx, 
+			const std::shared_ptr<ptx::PTXCompiler> compiler);
 		const std::size_t rootCount() const { return mRoots.size(); }
 		const std::vector<SceneRoot> roots() const { return mRoots; }
 		const SceneRoot root(const std::size_t& idx) const;
@@ -31,10 +38,37 @@ namespace tree {
 		optix::Context mContext;
 		optix::Group mGroup;			///< This is the tree (top node)
 		std::vector<SceneRoot> mRoots;
+		std::shared_ptr<rt::BoundingBoxProgram> mBoundingBoxProgram;
+		std::shared_ptr<rt::IntersectionProgram> mIntersectionProgram;
+		// TODO: make closest hit and anyhit program type objects
 		// who has the lights? probaly this guy here.
 		// Should the tree have the mapper?
 		// It would be cleaner
 	};
+
+	/**
+	*	Constructor
+	*/
+	SceneTree::SceneTree(
+		optix::Context& ctx,
+		const std::shared_ptr<ptx::PTXCompiler> compiler)
+		: mContext(ctx)
+	{
+		mBoundingBoxProgram =
+			std::make_shared<rt::BoundingBoxProgram>(
+				ctx,
+				std::string("mesh_bounds"),
+				std::string("res\\ptx\\triangle_mesh.ptx"),
+				std::string("..\\src\\CUDA\\triangle_mesh.cu"),
+				compiler);
+		mIntersectionProgram =
+			std::make_shared<rt::IntersectionProgram>(
+				ctx,
+				std::string("mesh_intersect"),
+				std::string("res\\ptx\\triangle_mesh.ptx"),
+				std::string("..\\src\\CUDA\\triangle_mesh.cu"),
+				compiler);
+	}
 
 	/**
 	*	adds root
@@ -78,6 +112,9 @@ namespace tree {
 		scene::Mesh m = s.meshes().back();
 		// get materials
 		std::vector<scene::Material> mats = s.materials();
+		// map primitives
+		// first make a Geometry Component
+
 	}
 
 }	//	!namespace tree
